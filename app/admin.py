8bda88acdbcbc7f4bd92bfa9a78190889f3cd36d
NekoSarada1101 <ryoyukikazu11@gmail.com>
2021-03-05 13:58:59 +0900

    feat: 管理サイトの設定

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from app.models import User
+
 
 # Register your models here.
+# admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["id", "mail", "password", "name"]
+    list_display_links = ["id", "mail"]
+
+
+admin.site.register(User, UserAdmin)
--- app/models.py
+++ app/models.py
@@ -5,3 +5,6 @@ class User(models.Model):
     mail = models.CharField("メールアドレス", max_length=255)
     password = models.CharField("パスワード", max_length=255)
     name = models.CharField("ユーザー名", max_length=255, default="user")
+
+    def __str__(self):
+        return self.mail
