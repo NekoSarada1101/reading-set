cceeb154d68027f0824ed74a9c1ea12815df91cd
NekoSarada1101 <ryoyukikazu11@gmail.com>
2021-03-05 13:45:10 +0900

    feat: マイグレーション

--- app/models.py
+++ app/models.py
@@ -1,2 +1,7 @@
 from django.db import models
 
+
+class Users(models.Model):
+    mail = models.CharField("メールアドレス", max_length=255)
+    password = models.CharField("パスワード", max_length=255)
+    name = models.CharField("ユーザー名", max_length=255, default="user")
